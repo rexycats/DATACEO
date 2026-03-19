07d839e3d7935383b201ca223e4e30ebaf66a1eb
rexycats <128919361+rexycats@users.noreply.github.com>
2026-03-19 18:16:38 +0100

    Delete init-lang.js

--- init-lang.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(function(){
-  window.LANG = window.LANG || "nl";
-  try {
-    const l = localStorage.getItem("datashop_lang");
-    if (l) window.LANG = l;
-  } catch(e) {}
-  if (typeof applyTranslations === "function") applyTranslations();
-})();
